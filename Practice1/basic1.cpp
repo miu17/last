3b2a42b57035e1d672b62806f303710e85f0fef7
美羽 菊地 <miu@sra.co.jp>
2023-07-04 17:26:45 +0900

    さらに

---
+++ Practice1/basic1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "π=" << 3.14159 << "..." << endl;
+	cout << 2 << '+' << 3 << '=' << 2 + 3 << endl;
+	cout << "9 /2  = " << 9.0 / 2 << endl;
+	cout << "9 % 2 = " << 9 % 2 << endl;
+	cout << "2 + 2 * 4 = " << 2 + 2 * 4 << endl;
+	cout << "(1 - 5) / 2 = " << (1 - 5) / 2 << endl;
+	return 0;
+}
+
---
+++ input1/input1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout << "何か値を入れてね > ";
+	cin >> a;
+
+	cout << "その数を３で割った余りは" << a % 3 << "です。" << endl;
+
+	return 0;
+}
